09f2f8d4e53814670caa7355466905fc04eacb55
Parrot85 <Christian@weist.net>
2024-07-30 22:34:03 -0700

    Mod

---
+++ src/main/java/net/silvercurve4746/tutorialmod/TutorialmodClient.java
@@ -0,0 +1,11 @@
+package net.silvercurve4746.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialmodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
